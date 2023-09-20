08f2bfec72a5160bc3adbe87d9ee03110610fb79
Jose Laurito <jigolaurito93@yahoo.com>
2023-09-20 03:48:17 -0500

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+# Import the Flask application
+from flask import Flask
+
+# Create instance of Flask class with the name "app"
+app = Flask(__name__)
+
+# import all of the routes from the routes.py file into the current package
+# (from the current directory, import routes.py)
+from . import routes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# If youre running the script, then run the application
+# if __name__ == '__main__':
+#     app.run()
+    # app.run(debug=True)
+
+
---
+++ app/routes.py
@@ -0,0 +1,19 @@
+# (from the app folder, import the instance of app inside the init.py file)
+from app import app
+# Import the render_template module from flask
+from flask import render_template
+
+# Create a decorator
+@app.route('/') #127.0.0.1:5000/
+def index():
+    # Renders the index.html file
+    # Takes 2 arguments, the name of the template AND the variables you want to pass to the template engine as keyword argument arguments
+    return render_template('index.html', name="Jose")
+
+@app.route('/signup') #127.0.0.1:5000/signup
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login') #127.0.0.1:5000/login
+def login():
+    return render_template('login.html')
