ad7b210b05acd1a437c505e4b28580151d4db180
Jose Laurito <jigolaurito93@yahoo.com>
2023-09-21 02:36:30 -0500

    added fake module

--- app/routes.py
+++ app/routes.py
@@ -2,13 +2,15 @@
 from app import app
 # Import the render_template module from flask
 from flask import render_template
+# import fake posts form fake data
+from fake_data import posts
 
 # Create a decorator
 @app.route('/') #127.0.0.1:5000/
 def index():
     # Renders the index.html file
     # Takes 2 arguments, the name of the template AND the variables you want to pass to the template engine as keyword argument arguments
-    return render_template('index.html', name="Jose")
+    return render_template('index.html', posts=posts)
 
 @app.route('/signup') #127.0.0.1:5000/signup
 def signup():
